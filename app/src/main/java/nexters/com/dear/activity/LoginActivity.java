2ef3e221578324b4c51f0115fc6675529fce1fc8
JunKyus <kuk941025@gmail.com>
2018-08-05 01:22:00 +0900

    login activity

--- app/src/main/java/nexters/com/dear/activity/LoginActivity.java
+++ app/src/main/java/nexters/com/dear/activity/LoginActivity.java
@@ -2,6 +2,9 @@ package nexters.com.dear.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import nexters.com.dear.R;
 
@@ -11,5 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
+
 }
