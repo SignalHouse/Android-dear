7001d53adb54c5c43f32ac8cf1230a709fb336f5
J2won <cjw2609@gmail.com>
2018-08-14 13:33:25 +0900

    register test

--- app/src/main/java/nexters/com/dear/activity/RegisterActivity.java
+++ app/src/main/java/nexters/com/dear/activity/RegisterActivity.java
@@ -8,11 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -41,55 +45,92 @@ public class RegisterActivity extends AppCompatActivity {
                 email = inputEmail.getText().toString();
                 nickname = inputNickname.getText().toString();
                 password = inputPassword.getText().toString();
+
+                new registDB().execute("http://192.168.43.31:8000/api/user");
             }
         });
     }
 
-    public class registDB extends AsyncTask<Void, Integer, Void> {
+    public class registDB extends AsyncTask<String, String, String> {
 
         @Override
-        protected Void doInBackground(Void... unused) {
-
-            String param = "u_id=" + email + "&u_name=" +nickname + "&u_pw=" + password + "";
+        protected String doInBackground(String... urls) {
 
             try {
-            /* 서버연결 */
-                URL url = new URL(getString(R.string.server_url));
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.connect();
 
-            /* 안드로이드 -> 서버 파라메터값 전달 */
-                OutputStream outs = conn.getOutputStream();
-                outs.write(param.getBytes("UTF-8"));
-                outs.flush();
-                outs.close();
+                //JSONObject를 만들고 key value 형식으로 값을 저장
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.accumulate("id","test");
+                jsonObject.accumulate("name", nickname);
+                jsonObject.accumulate("email",email);
+                jsonObject.accumulate("password",password);
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
 
-            /* 서버 -> 안드로이드 파라메터값 전달 */
-                InputStream is = null;
-                BufferedReader in = null;
-                String data = "";
+                try{
 
-                is = conn.getInputStream();
-                in = new BufferedReader(new InputStreamReader(is), 8 * 1024);
-                String line = null;
-                StringBuffer buff = new StringBuffer();
-                while ( ( line = in.readLine() ) != null )
-                {
-                    buff.append(line + "\n");
+                    URL url = new URL(urls[0]);
+
+                    //연결
+
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+
+                    con.connect();
+
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuffer buffer = new StringBuffer();
+                    String line = "";
+
+                    while((line = reader.readLine()) != null){
+
+                        buffer.append(line);
+
+                    }
+
+                    return buffer.toString();
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                } finally {
+
+                    if(con != null){
+                        con.disconnect();
+                    }
+
+                    try {
+
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-                data = buff.toString().trim();
-                Log.e("RECV DATA",data);
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
         }
-
     }
 }
