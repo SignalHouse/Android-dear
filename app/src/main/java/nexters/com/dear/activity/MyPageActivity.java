e96731ae265b6accad3ea6857a1da597a9b8b409
JunKyus <kuk941025@gmail.com>
2018-08-16 20:50:04 +0900

    editable nickname

--- app/src/main/java/nexters/com/dear/activity/MyPageActivity.java
+++ app/src/main/java/nexters/com/dear/activity/MyPageActivity.java
@@ -1,20 +1,28 @@
 package nexters.com.dear.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 import nexters.com.dear.R;
 
-public class MyPageActivity extends AppCompatActivity {
+public class MyPageActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.mypage_toolbar)
     Toolbar toolbar;
     @BindView(R.id.tool_bar_base_btn)
@@ -29,6 +37,17 @@ public class MyPageActivity extends AppCompatActivity {
     TextView txtChangePW;
     @BindView(R.id.mypage_dscrp_contact)
     TextView txtContact;
+    @BindView(R.id.mypage_txt_toggle_status)
+    TextView txtStatus;
+    @BindView(R.id.mypage_toggle)
+    Switch stNotification;
+    @BindView(R.id.mypage_txt_nickname)
+    EditText editNickname;
+    @BindView(R.id.mypage_img_edit)
+    ImageView imgEdit;
+    @BindView(R.id.mypage_layout)
+    View vLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +55,15 @@ public class MyPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_page);
 
         ButterKnife.bind(this);
+
         setToolbar();
+        setClickListener();
+    }
+
+    private void setClickListener(){
+        editNickname.setOnClickListener(this);
+        imgEdit.setOnClickListener(this);
+        vLayout.setOnClickListener(this);
     }
 
     private void setToolbar(){
@@ -47,6 +74,21 @@ public class MyPageActivity extends AppCompatActivity {
         txtTitle.setText("Setting");
 
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.mypage_txt_nickname:
+            case R.id.mypage_img_edit:
+                editNickname.setEnabled(true);
+                InputMethodManager mgr = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                mgr.showSoftInput(editNickname, InputMethodManager.SHOW_IMPLICIT);
+                break;
+            default:
+                editNickname.setEnabled(false);
+        }
+    }
+
     @OnClick(R.id.tool_bar_base_go_back)
     void onGoBackClicked(){
         finish();
@@ -59,12 +101,21 @@ public class MyPageActivity extends AppCompatActivity {
 
     @OnClick(R.id.mypage_dscrp_contact)
     void onContactClicked(){
-
+        Toast.makeText(getApplicationContext(), "Contact Clicked", Toast.LENGTH_SHORT).show();
     }
 
     @OnClick(R.id.mypage_dscrp_change_pw)
-    void onChangePWClikced(){
+    void onChangePWClicked(){
         Intent i = new Intent(MyPageActivity.this, PWUpdateActivity.class);
         startActivity(i);
+    }
+
+    @OnCheckedChanged(R.id.mypage_toggle)
+    void onCheckedChange(CompoundButton button, boolean checked){
+        editNickname.setEnabled(false);
+        if (checked)
+            txtStatus.setText("ON");
+        else
+            txtStatus.setText("OFF");
     }
 }
